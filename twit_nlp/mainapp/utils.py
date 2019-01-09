611dd5deb3301bcc2e59fa2187622eef429a411a
Korneliusz Pogorzelczyk <kor.pogorzelczyk@gmail.com>
2019-01-09 23:26:30 +0100

    added method converting query results to dictionary

--- twit_nlp/mainapp/utils.py
+++ twit_nlp/mainapp/utils.py
@@ -1,16 +1,27 @@
-from twython import Twython  
+from twython import Twython
 import json
+import pandas as pd
+
 
 def get_query(query_term):
     with open('twitter_credentials.json', 'r') as file:
         credentials = json.load(file)
-    
-    tweets = Twython(credentials['CONSUMER_KEY'], credentials['CONSUMER_SECRET'])
 
-    query = {'q': 'learn python',  
-        'result_type': 'popular',
-        'count': 100,
-        'lang': 'en',
-        }
+    tweets = Twython(credentials['CONSUMER_KEY'],
+                     credentials['CONSUMER_SECRET'])
+
+    query = {'q': query_term,
+             'result_type': 'popular',
+             'count': 100,
+             'lang': 'en',
+             }
 
     return tweets.search(**query)
+
+
+def tweets_to_dict(tweets):
+    tweets_dict = {'user': [], 'text': []}
+    for status in tweets['statuses']:
+        tweets_dict['user'].append(status['user']['name'])
+        tweets_dict['text'].append(status['text'])
+    return tweets_dict
