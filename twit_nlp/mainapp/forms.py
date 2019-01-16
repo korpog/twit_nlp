8503c4c848545338fde9795082e7e8a5ff23e6fc
Korneliusz Pogorzelczyk <kor.pogorzelczyk@gmail.com>
2019-01-16 09:55:12 +0100

    added form control for number of tweets queried

--- twit_nlp/mainapp/forms.py
+++ twit_nlp/mainapp/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 
+
 class QueryForm(forms.Form):
     query = forms.CharField(label='Twitter query', max_length=120)
+    num_of_tweets = forms.IntegerField(
+        label="Number of tweets", min_value=1, max_value=100)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['query'].widget.attrs.update(
             {'class': 'form-control', 'placeholder': 'Enter query here'})
+        self.fields['num_of_tweets'].widget.attrs.update(
+            {'class': 'form-control', 'placeholder': 'How many tweets?'})
--- twit_nlp/mainapp/urls.py
+++ twit_nlp/mainapp/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list_tweets, name='main'),
     path('results/', views.list_tweets, name='results'),
 ]
