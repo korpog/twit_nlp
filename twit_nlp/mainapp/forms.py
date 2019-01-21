571fbaeb94c2b6d4f8ee4ccb41da4bef083448d4
Korneliusz Pogorzelczyk <kor.pogorzelczyk@gmail.com>
2019-01-21 09:51:14 +0100

    added textarea input to text statistics

--- twit_nlp/mainapp/forms.py
+++ twit_nlp/mainapp/forms.py
@@ -2,9 +2,16 @@ from django import forms
 
 
 class TwitterForm(forms.Form):
+    RESULT_CHOICES = (
+        ('rec', 'recent'),
+        ('mix', 'mixed'),
+        ('pop', 'popular')
+    )
     query = forms.CharField(label='Twitter query', max_length=120)
     num_of_tweets = forms.IntegerField(
         label="Number of tweets", min_value=1, max_value=15)
+    result_type = forms.ChoiceField(
+        label="Result type", choices=RESULT_CHOICES)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -12,8 +19,13 @@ class TwitterForm(forms.Form):
             {'class': 'form-control', 'placeholder': 'Enter query here'})
         self.fields['num_of_tweets'].widget.attrs.update(
             {'class': 'form-control'})
+        self.fields['result_type'].widget.attrs.update(
+            {'class': 'form-control'})
+
 
 class StatsForm(forms.Form):
+    text = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Your message goes here'}))
     num_of_something = forms.IntegerField(
         label="Number of tweets", min_value=1, max_value=15)
 
